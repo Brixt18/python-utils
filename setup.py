8e3e9ab8cc995426a5ec09d376ff3f4248b974ce
Luca Mendez <lucamendez18@hotmail.com>
2023-04-06 16:30:34 -0300

    Change syntax on get_text_or_none

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='utils',
     packages=find_packages(include=["utils"]),
-    version='0.0.3',
+    version='0.0.4',
     description='Basics utils functions for Python',
     author='Brixt18',
     license='MIT',
