73cc1278bbb0e7ebefd56b3c3a89c1f8931c38ae
Luca Mendez <lucamendez18@hotmail.com>
2023-04-06 16:13:30 -0300

    Ln28 causaba un error aritmetico en el rotorno -> str | None

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='utils',
     packages=find_packages(include=["utils"]),
-    version='0.0.1',
+    version='0.0.2',
     description='Basics utils functions for Python',
     author='Brixt18',
     license='MIT',
